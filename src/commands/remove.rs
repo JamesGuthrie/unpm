c9e9d47cec9126e3ac88e7138c412e199c8fe4c6
James Guthrie <james@guthrie.ch>
2026-03-08 14:31:10 +0100

    feat: implement remove command

--- src/commands/remove.rs
+++ src/commands/remove.rs
@@ -1,3 +1,30 @@
-pub fn remove(_package: &str) -> anyhow::Result<()> {
-    todo!("implement remove command")
+use std::path::Path;
+
+use anyhow::bail;
+
+use crate::config::Config;
+use crate::lockfile::Lockfile;
+use crate::manifest::Manifest;
+use crate::vendor;
+
+pub fn remove(package: &str) -> anyhow::Result<()> {
+    let mut manifest = Manifest::load()?;
+    let mut lockfile = Lockfile::load()?;
+    let config = Config::load()?;
+
+    if manifest.dependencies.remove(package).is_none() {
+        bail!("Package '{}' not found in dependencies", package);
+    }
+
+    if let Some(locked) = lockfile.dependencies.remove(package) {
+        if let Some(filename) = locked.url.rsplit('/').next() {
+            vendor::remove_file(Path::new(&config.output_dir), filename)?;
+        }
+    }
+
+    manifest.save()?;
+    lockfile.save()?;
+
+    println!("Removed {package}");
+    Ok(())
 }
